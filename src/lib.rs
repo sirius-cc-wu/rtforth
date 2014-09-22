e35b487963d01a4918e9fa173a56b75997e91ffa
ccwu <ccwu660601@mapacode.tw>
2014-09-22 08:34:00 +0800

    CCWU: added name field to Word."

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,32 @@
 pub mod vm {
 
 // Word
-	pub struct Word {
+	pub struct Word<'a> {
 		is_immediate: bool,
+		name: &'a str,
 		action: fn(&VM)
 	}
 
-impl Word {
-	pub fn new(action: fn(&VM)) -> Box<Word> {
+impl<'a> Word<'a> {
+	pub fn new(name: &'a str, action: fn(&VM)) -> Box<Word<'a>> {
 		box Word {
 			is_immediate: false,
+			name: name,
 			action: action
 		}
 	}
 }
 
 // Virtual machine
-	pub struct VM {
+	pub struct VM<'a> {
 		is_paused: bool,
 		s_stack: Box<Vec<int>>,
 		r_stack: Box<Vec<int>>,
-		word_list: Box<Vec<Box<Word>>>
+		word_list: Box<Vec<Box<Word<'a>>>>
 	}
 
-	impl VM {
-		pub fn new() -> Box<VM> {
+	impl<'a> VM<'a> {
+		pub fn new() -> Box<VM<'a>> {
 			let mut vm = box VM {
 				is_paused: true,
 				s_stack: box Vec::with_capacity(16),
@@ -33,9 +35,9 @@ impl Word {
 			};
 			vm.s_stack.push(0);
 			vm.r_stack.push(0);
-			vm.word_list.push (Word::new(VM::noop));
-			vm.word_list.push (Word::new(VM::quit));
-			vm.word_list.push (Word::new(VM::bye));
+			vm.word_list.push (Word::new("noop", VM::noop));
+			vm.word_list.push (Word::new("quit", VM::quit));
+			vm.word_list.push (Word::new("bye", VM::bye));
 			vm
 		}
 

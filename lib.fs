851dde732e0bd3a5e1775e908297fb320ad01ec6
ccwu <ccwu660601@mapacode.tw>
2015-03-20 19:53:58 +0800

    CCWU: implemented command line.

--- lib.fs
+++ lib.fs
@@ -2,4 +2,3 @@ variable base
 10 base !
 : ?dup 0 <> if dup then ;
 marker empty
-words
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 extern crate jrforth;
 use jrforth::VM;
+use std::env;
 
 #[cfg(not(test))]
 fn main() {
-    hello::hello();
     let vm = &mut VM::new();
-    vm.load("lib.fs");
+    let args: Vec<_> = env::args().collect();
+    if args.len() > 1 {
+        if args[1] == "--help" {
+            println!("Usage: jrforth [options] [file]");
+            println!("JrForth will load lib.fs if no options and file is given.");
+            println!("Options:");
+            println!("  --help");
+        } else {
+            vm.load(&args[1]);
+        }
+    } else {
+        hello::hello();
+        vm.load("lib.fs");
+    }
 }
 
 #[cfg(not(test))]

b8b3f371d30bfb755702a490e70fa76a02938a1b
Sirius Wu <ccwu660601@gmail.com>
2015-08-07 21:52:28 +0800

    CCWU: replaced println with print for .s and words.

--- src/tools.rs
+++ src/tools.rs
@@ -24,14 +24,16 @@ impl Tools for ::core::VM {
     fn words(&mut self) {
         for w in self.word_list.iter() {
             let s = &self.n_heap[w.nfa..w.nfa+w.name_len];
-            println!("{}", s );
+            print!("{} ", s );
         }
+        println!("");
     }
 
     fn dot_s(&mut self) {
-        println!("<{}>", self.s_stack.len());
+        print!("<{}> ", self.s_stack.len());
         for s in self.s_stack.iter() {
-            println!("{}", s);
+            print!("{} ", s);
         }
+        println!("");
     }
 }

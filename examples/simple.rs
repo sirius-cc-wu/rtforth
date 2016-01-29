ba834e70afdaad28169ef080c772163d544b087c
Sirius Wu <ccwu660601@gmail.com>
2016-01-29 18:17:36 +0800

    added examples/simple.rs

---
+++ examples/simple.rs
@@ -0,0 +1,14 @@
+extern crate rtforth;
+use rtforth::core::VM;
+use rtforth::output::Output;
+
+// Evaluate "1 ."
+fn main() {
+    let vm = &mut VM::new(65536);
+    vm.add_output();
+    vm.set_source("1 .");
+    match vm.evaluate() {
+      Some(e) => println!("{:?}", e),
+      None => {}
+    }
+}

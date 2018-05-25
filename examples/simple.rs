0c0cc06d18aaa749f39bef19d73741c2625bd89d
Cheng-Chang Wu <ccwu660601@mapacode.tw>
2018-05-25 18:07:36 +0800

    fixed examples/simple.rs; removed exmples/multiuser.rs.

--- examples/simple.rs
+++ examples/simple.rs
@@ -5,7 +5,7 @@ use rtforth::vm::VM;
 // Evaluate "1 ."
 fn main() {
     let vm = &mut VM::new(100, 100);
-    vm.set_source("1 .");
+    vm.set_source("1 . flush");
     vm.evaluate();
     match vm.last_error() {
         Some(e) => println!("{:?}", e),

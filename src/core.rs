d48781ccb30958503eb4edde9dda7aa33a9fadd7
Cheng-Chang Wu <ccwu660601@mapacode.tw>
2018-07-13 09:55:55 +0800

    comment for backslash \.

--- src/core.rs
+++ src/core.rs
@@ -2111,6 +2111,7 @@ fn add_immediate_and_compile_only(&mut self, name: &str, action: primitive!{fn(&
         self.parse();
     }}
 
+    /// Begin a comment that includes the entire remainder of the current line.
     primitive!{fn imm_backslash(&mut self) {
         self.s_stack().push('\n' as isize);
         self.parse();

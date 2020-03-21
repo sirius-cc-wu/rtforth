0b398557f0699b22ecce23511986defb7a24f12d
Sirius Wu <ccwu660601@mapacode.tw>
2020-03-21 15:53:38 +0800

    fixed tc, and t,.

--- core.fs
+++ core.fs
@@ -81,8 +81,8 @@ variable >in  0 >in !
 
 \ Optimizer word list
 forth-wordlist optimizer-wordlist 2 set-order definitions
-: t, ( n -- )   there 1 cells allot ! ;
-: tc, ( char -- )   there 1 chars allot c! ;
+: t, ( n -- )   there 1 cells tallot ! ;
+: tc, ( char -- )   there 1 chars tallot c! ;
 forth-wordlist 1 set-order definitions
 
 \ Multitasker

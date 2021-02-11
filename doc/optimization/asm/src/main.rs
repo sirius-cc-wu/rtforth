715cff0f68f9ef5c9a16341e807da20ddbaaaf3d
Sirius Wu <ccwu660601@mapacode.tw>
2021-02-11 13:23:02 +0800

    Implement stack operation dup, swap, nip, drop, ?dup, over, rot, -rot, depth, pick, r>, >r, r@, rdepth, rpick in asm.

--- doc/optimization/asm/src/main.rs
+++ doc/optimization/asm/src/main.rs
@@ -3,49 +3,128 @@
 #[inline(never)]
 #[cfg(target_arch = "x86")]
 fn tinycore() {
-    // reg 0
-    let mut sp: u32 = 0;
-    // reg 1
-    let mut top: u32 = 0;
-    // reg 2
-    let mut tmp: u32 = 0;
-    // reg 3
     let stack = [0u32; 256];
+    let rstack = [0u32; 256];
+    let mut sp: u32 = 0;
+    let mut rp: u32 = 0;
+    let mut top: u32 = 0;
     unsafe {
         asm!(
-            // init sp
-            "mov {0}, {3}",
+            // init sp and rp.
+            "   mov {sp}, {stack}
+                mov {rp}, {rstack}
+                ret",
+            // sp@
+            "   sub {sp}, 4
+                mov [{sp}], {top}
+                mov {top}, {sp}
+                ret",
+            // sp!
+            "   mov {sp}, {top}
+                mov {top}, [{sp}]
+                add {sp}, 4
+                ret",
+            // rp@
+            "   sub {rp}, 4
+                mov [{rp}], {top}
+                mov {top}, {rp}
+                ret",
+            // rp!
+            "   mov {rp}, {top}
+                mov {top}, [{rp}]
+                add {rp}, 4
+                ret",
+            // ?dup
+            "   or {top}, {top}
+                jz 1f",
             // dup
-            "sub {0}, 4",
-            "mov [{0}], {1}",
-            "ret",
+            "   sub {sp}, 4
+                mov [{sp}], {top}
+            1:
+                ret",
             // drop
-            "mov {1}, [{0}]",
-            "add {0}, 4",
-            "ret",
+            "   mov {top}, [{sp}]",
             // nip
-            "add {0}, 4",
-            "ret",
+            "   add {sp}, 4
+                ret",
             // swap
-            "mov {2}, [{0}]",
-            "mov [{0}], {1}",
-            "mov {1}, {2}",
-            "ret",
+            "   mov {tmp}, [{sp}]
+                mov [{sp}], {top}
+                mov {top}, {tmp}
+                ret",
             // over
-            "sub {0}, 4",
-            "mov [{0}], {1}",
-            "mov {1}, 4[{0}]",
-            "ret",
+            "   sub {sp}, 4
+                mov [{sp}], {top}
+                mov {top}, 4[{sp}]
+                ret",
             // tuck
-            "mov {2}, [{0}]",
-            "sub {0}, 4",
-            "mov [{0}], {2}",
-            "mov 4[{0}], {1}",
-            "ret",
-            inout(reg) sp,
-            inout(reg) top,
-            inout(reg) tmp,
-            in(reg) &stack,
+            "   mov {tmp}, [{sp}]
+                sub {sp}, 4
+                mov [{sp}], {tmp}
+                mov 4[{sp}], {top}
+                ret",
+            // rot
+            "   mov {tmp}, {top}
+                mov {top}, 4[{sp}]
+                mov {tmp2}, [{sp}]
+                mov [{sp}], {tmp}
+                mov 4[{sp}], {tmp2}
+                ret",
+            // -rot
+            "   mov {tmp}, {top}
+                mov {top}, [{sp}]
+                mov {tmp2}, 4[{sp}]
+                mov 4[{sp}], {tmp}
+                mov [{sp}], {tmp2}
+                ret",
+            // pick
+            "   mov {top}, [{top}*4][{sp}]
+                ret",
+            // depth
+            // TODO: 可能有差一的錯誤
+            "   sub {sp}, 4
+                mov [{sp}], {top}
+                lea {top}, 256[{stack}]
+                sub {top}, {sp}
+                sar {top}
+                sar {top}
+                ret",
+            // >r
+            "   sub {rp}, 4
+                mov [{rp}], {top}
+                mov {top}, [{sp}]
+                add {sp}, 4
+                ret"
+            // r>
+            "   sub {sp}, 4
+                mov [{sp}], {top}
+                mov {top}, [{rp}]
+                add {rp}, 4
+                ret"
+            // r@
+            "   sub {sp}, 4
+                mov [{sp}], {top}
+                mov {top}, [{rp}]
+                ret"
+            // rdepth
+            // TODO: 可能有差一的錯誤
+            "   sub {sp}, 4
+                mov [{sp}], {top}
+                lea {top}, 256[{rstack}]
+                sub {top}, {rp}
+                sar {top}
+                sar {top}
+                ret",
+            // rpick
+            "   mov {top}, [{top}*4][{rp}]
+                ret",
+            stack = in(reg) &stack,
+            rstack = in(reg) &rstack,
+            sp = inout(reg) sp,
+            rp = inout(reg) rp,
+            top = inout(reg) top,
+            tmp = out(reg) _,
+            tmp2 = out(reg) _,
         );
     }
 }

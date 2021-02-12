eb86c7c1d66ebae032f9b6bdb4b49993edbcfe8c
Sirius Wu <ccwu660601@mapacode.tw>
2021-02-13 00:39:47 +0800

    Implement u/mod /mod mod / 2* 2/ in asm.

--- doc/optimization/asm/src/main.rs
+++ doc/optimization/asm/src/main.rs
@@ -8,6 +8,8 @@ fn tinycore() {
     let mut sp: u32 = 0;
     let mut rp: u32 = 0;
     let mut top: u32 = 0;
+    let mut tmp: u32 = 0;
+    let mut tmp2: u32 = 0;
     // 由於無法得到以下各指令的位址，此一作法適合用來分析，瞭解 rust 如何配 registers。
     // 可以於完成後反組譯成 .s 檔後加上 labels，再使用 gas 編譯成 .so 檔。
     unsafe {
@@ -19,141 +21,179 @@ fn tinycore() {
             // stackjugglers
             // sp@
             "   sub {sp}, 4
-                mov [{sp}], {top}
-                mov {top}, {sp}
+                mov [{sp}], ebx
+                mov ebx, {sp}
                 ret",
             // sp!
-            "   mov {sp}, {top}
-                mov {top}, [{sp}]
+            "   mov {sp}, ebx
+                mov ebx, [{sp}]
                 add {sp}, 4
                 ret",
             // rp@
             "   sub {sp}, 4
-                mov [{rp}], {top}
-                mov {top}, {rp}
+                mov [{rp}], ebx
+                mov ebx, {rp}
                 ret",
             // rp!
-            "   mov {rp}, {top}
-                mov {top}, [{sp}]
+            "   mov {rp}, ebx
+                mov ebx, [{sp}]
                 add {sp}, 4
                 ret",
             // ?dup
-            "   or {top}, {top}
+            "   or ebx, ebx
                 jz 1f",
             // dup
             "   sub {sp}, 4
-                mov [{sp}], {top}
+                mov [{sp}], ebx
             1:  ret",
             // drop
-            "   mov {top}, [{sp}]",
+            "   mov ebx, [{sp}]",
             // nip
             "   add {sp}, 4
                 ret",
             // swap
-            "   mov {tmp}, [{sp}]
-                mov [{sp}], {top}
-                mov {top}, {tmp}
+            "   mov eax, [{sp}]
+                mov [{sp}], ebx
+                mov ebx, eax
                 ret",
             // over
             "   sub {sp}, 4
-                mov [{sp}], {top}
-                mov {top}, 4[{sp}]
+                mov [{sp}], ebx
+                mov ebx, 4[{sp}]
                 ret",
             // tuck
-            "   mov {tmp}, [{sp}]
+            "   mov eax, [{sp}]
                 sub {sp}, 4
-                mov [{sp}], {tmp}
-                mov 4[{sp}], {top}
+                mov [{sp}], eax
+                mov 4[{sp}], ebx
                 ret",
             // rot
-            "   mov {tmp}, {top}
-                mov {top}, 4[{sp}]
-                mov {tmp2}, [{sp}]
-                mov [{sp}], {tmp}
-                mov 4[{sp}], {tmp2}
+            "   mov eax, ebx
+                mov ebx, 4[{sp}]
+                mov edx, [{sp}]
+                mov [{sp}], eax
+                mov 4[{sp}], edx
                 ret",
             // -rot
-            "   mov {tmp}, {top}
-                mov {top}, [{sp}]
-                mov {tmp2}, 4[{sp}]
-                mov 4[{sp}], {tmp}
-                mov [{sp}], {tmp2}
+            "   mov eax, ebx
+                mov ebx, [{sp}]
+                mov edx, 4[{sp}]
+                mov 4[{sp}], eax
+                mov [{sp}], edx
                 ret",
             // pick
-            "   mov {top}, [{top}*4][{sp}]
+            "   mov ebx, [ebx*4][{sp}]
                 ret",
             // depth
             // TODO: 可能有差一的錯誤
             "   sub {sp}, 4
-                mov [{sp}], {top}
-                lea {top}, 256[{stack}]
-                sub {top}, {sp}
-                sar {top}
-                sar {top}
+                mov [{sp}], ebx
+                lea ebx, 256[{stack}]
+                sub ebx, {sp}
+                sar ebx
+                sar ebx
                 ret",
             // >r
             "   sub {rp}, 4
-                mov [{rp}], {top}
-                mov {top}, [{sp}]
+                mov [{rp}], ebx
+                mov ebx, [{sp}]
                 add {sp}, 4
                 ret",
             // r>
             "   sub {sp}, 4
-                mov [{sp}], {top}
-                mov {top}, [{rp}]
+                mov [{sp}], ebx
+                mov ebx, [{rp}]
                 add {rp}, 4
                 ret",
             // r@
             "   sub {sp}, 4
-                mov [{sp}], {top}
-                mov {top}, [{rp}]
+                mov [{sp}], ebx
+                mov ebx, [{rp}]
                 ret",
             // rdepth
             // TODO: 可能有差一的錯誤
             "   sub {sp}, 4
-                mov [{sp}], {top}
-                lea {top}, 256[{rstack}]
-                sub {top}, {rp}
-                sar {top}
-                sar {top}
+                mov [{sp}], ebx
+                lea ebx, 256[{rstack}]
+                sub ebx, {rp}
+                sar ebx
+                sar ebx
                 ret",
             // rpick
-            "   mov {top}, [{top}*4][{rp}]
+            "   mov ebx, [ebx*4][{rp}]
                 ret",
             // calculations
             // +
-            "   add {top}, [{sp}]
+            "   add ebx, [{sp}]
                 add {sp}, 4
                 ret",
             // -
-            "   sub {top}, [{sp}]
+            "   sub ebx, [{sp}]
                 add {sp}, 4
                 ret",
             // 1-
-            "   dec {top}
+            "   dec ebx
                 ret",
             // 1+
-            "   inc {top}
+            "   inc ebx
                 ret",
             // 2-
-            "   sub {top}, 2
+            "   sub ebx, 2
                 ret",
             // 2+
-            "   add {top}, 2
+            "   add ebx, 2
                 ret",
             // abs
-            "   or {top}, {top}
+            "   or ebx, ebx
                 jns 1f",
             // negate
-            "   neg {top}
+            "   neg ebx
             1:  ret",
+            // u/mod
+            "   mov eax, [{sp}]
+                xor edx, edx
+                div ebx
+                mov ebx, eax
+                mov [{sp}], edx
+                ret",
+            // /mod
+            "   mov eax, [{sp}]
+                cdq
+                idiv ebx
+                mov ebx, eax
+                mov [{sp}], edx
+                ret",
+            // mod
+            "   mov eax, [{sp}]
+                cdq
+                idiv ebx
+                mov ebx, edx
+                add {sp}, 4
+                ret",
+            // /
+            "   mov eax, [{sp}]
+                cdq
+                idiv ebx
+                mov ebx, eax
+                add {sp}, 4
+                ret",
+            // *
+            "   imul ebx, [{sp}]
+                add {sp}, 4
+                ret",
+            // 2*
+            "   shl ebx
+                ret",
+            // 2/
+            "   sar ebx
+                ret",
             stack = in(reg) &stack,
             rstack = in(reg) &rstack,
             sp = inout(reg) sp,
             rp = inout(reg) rp,
-            top = inout(reg) top,
-            tmp = out(reg) _,
-            tmp2 = out(reg) _,
+            inout("ebx") top,
+            inout("eax") tmp,
+            inout("edx") tmp2,
         );
     }
 }
